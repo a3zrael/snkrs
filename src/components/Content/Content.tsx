021e233c751dd202aa35ab945278f514031422a1
Andrey_Marusov <marusov140@gmail.com>
2023-07-24 17:49:41 +0300

    fix

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,8 +1,13 @@
 import './Content.scss'
 import lupa from './img/lupa.svg'
+import cros1 from './img/cros1.svg'
+import cros2 from './img/cros2.svg'
 import ItemCard from '../ui/itemCard/itemCard';
 
 const Content = () => {
+
+
+
     return(
         <div className='section__content'> 
             <div className="search">
@@ -13,11 +18,10 @@ const Content = () => {
                 </div>
             </div>
             <div className='wrap_snkrs'>
-                <ItemCard/>
-                <ItemCard/>
-                <ItemCard/>
-                
-                
+                <ItemCard cros={cros1} name='Мужские Кроссовки Nike Blazer Mid Suede' price={12999}/>
+                <ItemCard cros={cros2} name='Мужские Кроссовки Nike Air Max 270' price={15700}/>
+                <ItemCard cros={cros2} name='Мужские Кроссовки Nike Air Max 270' price={15700}/>
+                <ItemCard cros={cros1} name='Мужские Кроссовки Nike Blazer Mid Suede' price={12999}/>
             </div>
             
         </div>
--- src/components/ui/itemCard/itemCard.tsx
+++ src/components/ui/itemCard/itemCard.tsx
@@ -1,29 +1,32 @@
 import './itemCard.scss'
 import plus from './img/plus.svg'
 import greenPlus from './img/greenPlus.svg'
-import cros from './img/cros1.svg'
 import liked from './img/liked.svg'
 import activeLiked from './img/activeLiked.svg'
 
+interface itemCardProps {cros:string, name:string, price:number}
 
-const itemCard = () => {
+const itemCard = ({cros, name, price}: itemCardProps) => {
     return(
         <div className='card'>
             <div className='favorite'>
                 <img src={liked} alt="like" />     
             </div>
-            <img className='cros' src={cros} alt="snkrs" />
-            <p className='name'>Мужские Кроссовки Nike Blazer Mid Suede</p> 
-            <div className='plus_basket'>
-                <div className='info'>
-                    <span className='price'>Цена:</span>
-                    <span className='sum_price'>12 900 rub</span>
+                <div className='img_box'>
+                    <img className='cros' src={cros} alt="snkrs" />
                 </div>
-                <button>
-                    <img className='plus' src={plus} alt="plus" />
-                    <img className='plus_active' src={greenPlus} alt="" />
-                </button>
-            </div>
+                <p className='name'>{name}</p> 
+                <div className='plus_basket'>
+                    <div className='info'>
+                        <span className='price'>Цена:</span>
+                        <span className='sum_price'>{price} rub</span>
+                    </div>
+                    <button>
+                        <img className='plus' src={plus} alt="plus" />
+                        <img className='plus_active' src={greenPlus} alt="" />
+                    </button>
+                    
+                </div>
         </div>
     )
 }
