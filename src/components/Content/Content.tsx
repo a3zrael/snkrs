01123ea7e14b0f946d37264ea41604fe962a7a4d
Andrey_Marusov <marusov140@gmail.com>
2023-08-03 16:32:27 +0300

    fix

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,5 +1,6 @@
 import './Content.scss';
 import lupa from './img/lupa.svg';
+import krest from './img/krest.svg';
 import ItemCard from '../ui/itemCard/itemCard.tsx';
 import { useEffect, useState } from 'react';
 
@@ -8,8 +9,7 @@ interface ItemCardProps {
 }
 
 const Content = ({ setItemsBasket }: ItemCardProps) => {
-
-	const [searchValue,setSearchValue] = useState('')
+	const [searchValue, setSearchValue] = useState('');
 
 	const [items, setItems] = useState<
 		{
@@ -33,17 +33,25 @@ const Content = ({ setItemsBasket }: ItemCardProps) => {
 		setItemsBasket((prev: any) => [...prev, obj]);
 	};
 
-	const onChangeSearchInput = (event: any) =>{
+	const onChangeSearchInput = (event: any) => {
 		setSearchValue(event.target.value);
-	} 
+	};
 
 	return (
 		<div className="section__content">
 			<div className="search">
-				<h3 className="title">{searchValue ? `Поиск по запросу: "${searchValue}"`: "Все кроссовки"}</h3>
+				<h3 className="title">
+					{searchValue ? `Поиск по запросу: "${searchValue}"` : 'Все кроссовки'}
+				</h3>
 				<div className="box__input">
-					<img src={lupa} alt="" />
-					<input onChange={onChangeSearchInput} type="text" placeholder="Поиск..." />
+				<img className='lupa' src={lupa} alt="" />
+					{searchValue && <img className='clear' src={krest} onClick={() => setSearchValue('')}/>}
+					<input
+						onChange={onChangeSearchInput}
+						type="text"
+						placeholder="Поиск..."
+						value={searchValue}
+					/>
 				</div>
 			</div>
 			<div className="wrap_snkrs">
